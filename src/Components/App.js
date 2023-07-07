16120f64c5f1158afd2fea61815afe0081f6fe8e
Martin <martingarcia804@yahoo.com>
2023-07-07 10:23:57 -0500

    Delete filler code/boiler plate code

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MovieList from "./MovieList.js"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello world!!</h1>
+    </div>
+  );
+}
+
+export default App;
