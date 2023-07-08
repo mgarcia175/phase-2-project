dac0ea4b2f70425b8c0f425d80e84e555f8765de
Martin <martingarcia804@yahoo.com>
2023-07-08 01:57:03 -0500

    API to add movies implemented and pass down to MovieList component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieList from "./MovieList.js"
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      "Title": "Star Wars: Episode IV - A New Hope",
+      "Year": "1977",
+      "imdbID": "tt0076759",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+    }
+  ])
 
   return (
     <div>
-      <h1>Hello world!!</h1>
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function MovieList() {
+function MovieList(props) {
 
     return(
         <div>
-
+            {props.movies.map((movie, index) => (
+            <div key={index}>
+                <img src={movie.Poster} alt="Poster of movie"></img>
+            </div>
+            ))}
         </div>
     )
 }
