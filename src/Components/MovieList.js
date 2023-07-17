e66cdee7b4ce6070f13d63b4451fd29beeed150f
Martin <martingarcia804@yahoo.com>
2023-07-16 21:53:43 -0500

    Add initial grid styling for movies. Still need grid placement as they are just in line right now.

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,10 +3,10 @@ import React from "react";
 function MovieList(props) {
 
     return(
-        <div>
+        <div className="card-container">
             {props.movies.map((movie, index) => (
-            <div key={index}>
-                <img src={movie.Poster} alt="Poster of movie"></img>
+            <div className="movie-style" key={index}>
+                <img src={movie.Poster} alt="Poster of movie" id="img"></img>
             </div>
             ))}
         </div>
