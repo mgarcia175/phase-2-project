edb8079577717ddcec4dd8bc735423addc677257
Martin <martingarcia804@yahoo.com>
2023-07-18 19:53:28 -0500

    Fixed loading issue. To use Browser Router, needed to sorround with tags in index.js.. have also initialized the routes with nav bar buttons and error message

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,65 +1,52 @@
 import React, { useState } from "react";
-import { Switch, Route } from "react-router-dom";
 import MyReviews from "./MyReviews";
 import MyWatchList from "./MyWatchList";
 import MyFlops from "./MyFlops";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function NavBar() {
 
   const [page, setPage] = useState('/')
 
     return (
-
-      <div>
-        <NavBar on changePage={setPage} />
-        <Switch>
-          <Route path='/my-reviews'>
-                <MyReviews />
-            </Route>
-
-            <Route path='/watch-list'>
-                <MyWatchList />
-            </Route>
-
-            <Route path='/my-flops'>
-                <MyFlops />
-            </Route>
-
-            <Route path='*'>
-                <h1>404 Not Found</h1>
-            </Route>
-        </Switch>
-      </div>
-
-
-
-      //   <Router>
-      //   <div>
-      //     <ul>
-      //       <li>
-      //         <Link to="/">All Movies</Link>
-      //       </li>
-      //       <li>
-      //         <Link to="/watchlist">My Watch List</Link>
-      //       </li>
-      //       <li>
-      //         <Link to="/reviews">My Reviews</Link>
-      //       </li>
-      //     </ul>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/my-reviews">My Reviews</Link>
+            </li>
+            <li>
+              <Link to="/watchlist">My Watch List</Link>
+            </li>
+            <li>
+              <Link to="/my-flops">My Flops</Link>
+            </li>
+          </ul>
   
-      //     <Switch>
-      //       <Route path="/watchlist">
-      //         <WatchList />
-      //       </Route>
-      //       <Route path="/reviews">
-      //         <Reviews />
-      //       </Route>
-      //       <Route path="/">
-      //         <Home />
-      //       </Route>
-      //     </Switch>
-      //   </div>
-      // </Router>
+          <Switch>
+              <Route path='/my-reviews'>
+                  <MyReviews />
+              </Route>
+
+              <Route path='/watch-list'>
+                  <MyWatchList />
+              </Route>
+
+              <Route path='/my-flops'>
+                  <MyFlops />
+              </Route>
+
+              <Route path='*'>
+                  <h1 id='error'>No movies.. it's so cold.. </h1>
+              </Route>
+          </Switch>
+        </div>
+      </Router>
     )
 }
 
